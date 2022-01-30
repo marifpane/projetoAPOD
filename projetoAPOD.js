32344df6dcf5fe238c8ceeb164cf50500be402d8
marifpane <96240207+marifpane@users.noreply.github.com>
2022-01-30 14:04:26 -0300

    funcionando

--- projetoAPOD.js
+++ projetoAPOD.js
@@ -8,10 +8,13 @@ $("form").submit(function (event) {
     $.ajax({
         url: `https://api.nasa.gov/planetary/apod?api_key=qB1MOvbomL5FCXN0Agy8otHAKdV8afAXUH0ykbhd&date=${data}`,
         success: function (data){
+            console.log(data.media_type);
+            console.log(data.url);
+            $("#dataTirada").text(data.date);
             $("#imagem").attr(`src`, data.url);
             $("#titulo").text(data.title);
             $("#explicar").text(data.explanation);
-            $("#autor").text(data.autor);
+            $("#autor").text(data.copyright);
 
         },
     })
